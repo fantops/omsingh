837e4bf5e8af05e423a0d714c6f52406f4e2168b
Om Prakash Singh <om.prakash.singh@wdc.com>
2020-05-17 02:16:45 +0530

    small changes in stylings

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
   Have focus outline only for keyboard users 
  ---------------------------------------- */
 
+
 const handleFirstTab = (e) => {
   if(e.key === 'Tab') {
     document.body.classList.add('user-is-tabbing')
